a594006c1e902f731a0b930e4465332a1fc216c2
Andrea Berardi <andrea.berardi@hotmail.com>
2023-03-04 20:09:52 -0300

    Creando el repositorio en GitHub

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Alumna: Andrea Berardi (the best)
         </p>
+        <h2>Coderhouse inc.</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendamos React
         </a>
       </header>
     </div>
   );
+
 }
 
 export default App;
