1d148d7738bd59aaef9012d3deff30af093d2113
Andrea Berardi <andrea.berardi@hotmail.com>
2023-03-05 20:31:05 -0300

    Estructura inicial del proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+// import saludar from './Utils.js'
+// import { saludar } from './Utils.js'
+// import { saludar, chau } from './Utils.js'
 
 function App() {
+
+  // saludar()
+  // chau()
+
+  const prof = "Mauro Paul"
+
+  // const mensaje = `El profesor es ${prof}`
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Alumna: Andrea Berardi (the best)
+          {/* Alumna: Andrea Berardi (the best) */}
+          {/* {prof} */}
+          {/* {mensaje} */}
+          El profesor es {prof}
         </p>
         <h2>Coderhouse inc.</h2>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Aprendamos React
-        </a>
+        </a> */}
       </header>
+
+
     </div>
   );
 
